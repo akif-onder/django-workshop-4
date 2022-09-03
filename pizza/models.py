c7525f8634dfe53bbfb026a25ecd56e0e78fabf3
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-09-03 17:21:03 +0300

    end

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+SIZE = [  
+        ('Sm', 'Small'),
+        ('Md', 'Medium'),
+        ('Lg', 'Large'),
+    ]
+class Size(models.Model):
+   
+    title = models.CharField(max_length=100)
+    size = models.CharField( max_length=10, choices=SIZE)
+    
+
+    def __str__(self):
+        return self.title # This is for good visual experimentation!
+
+
+class Pizza(models.Model):
+    topping1 = models.CharField(max_length=100)
+    topping2 = models.CharField(max_length=100) 
+    size = models.ForeignKey(Size, on_delete=models.CASCADE) # This is for correlation to the Size class
+    
+    def __str__(self):
+        return self.size, self.topping1, self.topping2
